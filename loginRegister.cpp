8924a23b9e097caeeaed2e9511f390969c432b39
bowenzhao <bzhao19@ucsc.edu>
2023-09-15 01:16:27 -0400

    valid username and password

--- loginRegister.cpp
+++ loginRegister.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include<unordered_map>
+#include<unordered_set>
 
 using namespace std;
 
@@ -18,7 +20,38 @@ bool isLoggedIn() {
     return un == userName && pw == password;
 }
 
+bool validUserName(string str) {
+    for(auto &ch : str) {
+        if((ch < 'a' || ch > 'z')
+        && (ch < 'A' || ch > 'Z')
+        && (ch < '0' || ch > '9')
+        || ch == ' ') {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool validPassword(string str) {
+    string special = "!?@#$%&*<>,.";
+    unordered_set<char> speicalSet;
+    for(auto &ch : special) {
+        speicalSet.insert(ch);
+    }
+    for(auto &ch : str) {
+        if((ch < 'a' || ch > 'z')
+        && (ch < 'A' || ch > 'Z')
+        && (ch < '0' || ch > '9')
+        && (!speicalSet.count(ch))
+        || ch == ' ') {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
+    unordered_map<string, ofstream> data;
     char choice;
     cout << "Enter your choice:\n1.Login\n2.Register" << endl;
     cin >> choice;
@@ -35,10 +68,21 @@ int main() {
     }
     else if(choice == '2') {
         string userName, password;
-        cout << "Enter A Valid User Name: ";
-        cin >> userName;
-        cout << "Enter A Valid Password: ";
-        cin >> password;
+        bool validUn = false, validPw = false;
+        while(!validUn) {
+            cout << "Enter A Valid User Name:\n" 
+            << "(the user name should only contain lower case, upper case letter, and digits)" << endl;
+            cin >> userName;
+            cout << userName << endl;
+            validUn = validUserName(userName);
+        }
+        while(!validPw) {
+            cout << "Enter A Valid Password:\n"
+            << "(the password should only contain lower case, upper case letter, digits, and special characters !?@#$%&*<>,.)" << endl;
+            cin >> password;
+            cout << password << endl;
+            validPw = validPassword(password);
+        }
 
         ofstream file;
         file.open(userName + ".txt");
